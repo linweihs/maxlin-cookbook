73bd5a65f8daa128e1becfb60f3fe918e4ae9b26
maxlin <maxlin@yahoo-inc.com>
2017-05-12 17:13:29 -0700

    first commit

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source 'https://supermarket.chef.io'
+
+# cookbook 'chef-client', '~> 4.6.0'
+
+metadata
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'maxlin-cookbook'
+maintainer       'maxlin'
+maintainer_email 'max.linweihs@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures maxlin-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+# use other cookbook's recipe, e.g
+# depends        'php'
